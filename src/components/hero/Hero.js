369212ab9bc62b239dba693f57ef29321bd52f3f
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-06-18 08:44:01 +0530

    header and inline style

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -12,9 +12,12 @@ const Hero = ({ movies }) => {
       <Carousel>
         {movies.map((movie) => {
           return (
-            <Paper>
+            <Paper key={movie.id}>
               <div className="movie-card-container">
-                <div className="movie-card">
+                <div
+                  className="movie-card"
+                  style={{ "--img": `url(${movie.backdrops[0]})` }}
+                >
                   <div className="movie-detail">
                     <div className="movie-poster">
                       <img src={movie.poster} alt="" />
